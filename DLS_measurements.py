df92a24de6c2715f15c124fb56367c1435230751
vidal05 <vidfloatson@gmail.com>
2017-05-26 18:51:34 -0500

    plotting data

---
+++ DLS_measurements.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+from matplotlib import pyplot as plt
+import numpy as np
+
+filename = sys.argv[1]
+f=open(filename)
+reader = list(csv.reader(f,delimiter=','))
+columns = len(reader[0])
+x = np.log([float(row[0]) for row in reader if not str(row[0][0]).isalpha()])
+y=[[row[i] for row in reader if not str(row[1][0]).isalpha()] for i in range(1,columns)]
+for i in range(len(y)):
+	plt.scatter(x,y[i],label="med%s" % i )
+
+plt.legend(loc="upper right")	
+plt.show()
